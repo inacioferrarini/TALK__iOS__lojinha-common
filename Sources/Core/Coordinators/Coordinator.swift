a402b39d48183ab077a40100dcda44360fb9e4c7
Inacio Ferrarini <inacio.ferrarini@accenture.com>
2019-07-17 15:50:01 -0300

    Removes files

--- Sources/Core/Coordinators/Coordinator.swift
+++ Sources/Core/Coordinators/Coordinator.swift
@@ -8,25 +8,8 @@
 
 import UIKit
 
-protocol Coordinator: class {
-    
-    var childCoordinators: [Coordinator] { get set }
-    var navigationController: UINavigationController { get set }
+public protocol Coordinator: class {
     
     func start()
-    func childDidFinish(_ child: Coordinator?)
     
-}
-
-extension Coordinator {
-    
-    func childDidFinish(_ child: Coordinator?) {
-        for (index, coordinator) in childCoordinators.enumerated() {
-            if coordinator === child {
-                childCoordinators.remove(at: index)
-                break
-            }
-        }
-    }
-
 }
